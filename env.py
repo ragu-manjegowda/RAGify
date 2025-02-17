ceab475c0d291fda2a758e01d1361b4e25311c64
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2025-02-17 13:12:03 -0800

    [chore]: warnings, disabled
    Details:
        Disable warning messages from LangChain.
    
    Resolves:
     * None
    
    Tests:
     * N/A
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- env.py
+++ env.py
@@ -1,7 +1,10 @@
 """Environment variables."""
 
-REPO_PATH = "./code"
-VECTOR_STORE_PATH = "./vector_store"
-VECTORIZE_CODEBASE = False
+REPO_PATH = "./vsomeip"
+VECTOR_STORE_PATH = "./.vector_store"
+VECTORIZE_CODEBASE = True
 VECTORIZE_FILE_TYPES = ["cpp", "md", "py"]
 LLM_MODEL = "nvdev/meta/llama-3.3-70b-instruct"
+EMBED_MODEL = "nvdev/nvidia/llama-3.2-nv-embedqa-1b-v2"
+TOKENIZER_MODEL = "llama-3.3-70b-instruct"
+TOKEN_LIMIT = 32768
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 """RAG on Codebase with py, cpp, md files using Langchain."""
 
+import sys
 import time
+import warnings
 
 from rag import (
     compile_and_get_graph
@@ -10,6 +12,9 @@ from vectorize import (
 )
 
 
+# Ignore all warnings
+warnings.filterwarnings("ignore")
+
 # Vectorize codebase, Note: controlled by env variable
 vectorize_codebase()
 
@@ -18,13 +23,25 @@ graph = compile_and_get_graph()
 
 
 # Function to run RAG
-query = input("Enter query: ")
+while True:
+    # Ignore empty queries
+    query = None
+    print("\n\n======================================")
+    while not query:
+        query = input("\nEnter query: ")
 
-for message, metadata in graph.stream(
-    {"messages": [{"role": "user", "content": query}]},
-    stream_mode="messages"
-):
-    # Terminal users deserve some pretty printing of the response
-    for char in message.content:
-        print(char, end="", flush=True)
-        time.sleep(0.01)
+    if query == "exit" or query == "quit":
+        sys.exit(0)
+
+    print("\n======================================\n")
+    try:
+        for message, metadata in graph.stream(
+            {"messages": [{"role": "user", "content": query}]},
+            stream_mode="messages"
+        ):
+            # Terminal users deserve some pretty printing of the response
+            for char in message.content:
+                print(char, end="", flush=True)
+                time.sleep(0.01)
+    except Exception as e:
+        print(f'Error in RAG {str(e)}')
